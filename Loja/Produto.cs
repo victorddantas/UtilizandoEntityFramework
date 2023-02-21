75f85506267928f4ff0ae55b5072517f60480a97
Victor Domingues Dantas <vic.d.dantas61@gmail.com>
2023-02-21 15:43:22 -0300

    Add project files.

---
+++ Loja/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja
+{
+    internal class Produto
+    {
+        public int Id { get; internal set; }
+        public string Nome { get; internal set; }
+        public string Categoria { get; internal set; }
+        public double Preco { get; internal set; }
+    }
+}
